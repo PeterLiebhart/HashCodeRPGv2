94b41781a6698db1dae293f373434272c97d672f
Peter Liebhart <peterliebhart.business@gmail.com>
2024-03-09 20:14:11 +0100

    added callbacks for applyDamage, applyHeal and isDead methods. These callbacks will be used in the GUI and other parts of the game

--- src/serviceClasses/Combat.java
+++ src/serviceClasses/Combat.java
@@ -4,21 +4,46 @@ import dataClasses.Entity;
 
 public class Combat {
 
-    //Method to calculate the damage of an attack
-    public static int calculateDamage(int AP, int armor) {
-        return AP - armor;
-    }
-    //Method to apply the damage to the entity
-    public static void applyDamage(int damage, Entity entity) {
-        entity.setHP(entity.getHP() - damage);
-    }
-    //Method to check if the entity is dead
-    public static boolean isDead(Entity entity) {
-        return entity.getHP() <= 0;
-    }
-    //Method to heal the entity by a certain amount
-    public static void heal(int amount, Entity entity) {
-        entity.setHP(entity.getHP() + amount);
+    // Callback interface
+    public interface CombatCallback {
+        void onDamageApplied(int damage, Entity entity);
+        void onHealApplied(int amount, Entity entity);
+        void onEntityDied(Entity entity);
     }
 
+    private CombatCallback callback;
+
+    public Combat(CombatCallback callback) {
+        this.callback = callback;
+    }
+
+    //Method to calculate the damage of an attack
+    public int calculateDamage(int AP, int armor) {
+        return AP - armor;
+    }
+
+    //Method to apply the damage to the entity
+    public void applyDamage(int damage, Entity entity) {
+        entity.setHP(entity.getHP() - damage);
+        callback.onDamageApplied(damage, entity);
+        if (isDead(entity)) {
+            callback.onEntityDied(entity);
+        }
+    }
+
+    //Method to heal the entity by a certain amount
+    public void applyHeal(int amount, Entity entity) {
+        entity.setHP(entity.getHP() + amount);
+        callback.onHealApplied(amount, entity);
+    }
+
+    //Method to check if the entity is dead
+    public boolean isDead(Entity entity) {
+        return entity.getHP() <= 0;
+    }
+
+    //Method to check if the entity is at full health
+    public boolean isAtFullHealth(Entity entity) {
+        return entity.getHP() == 100;
+    }
 }
